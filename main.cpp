2e87827100d4fb4ba988306f4f8125288a0a941d
IFriendlyI <82224993+IFriendlyI@users.noreply.github.com>
2024-10-30 15:08:22 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+  SetConsoleCP(1251);
+  SetConsoleOutputCP(1251);
+
+  // here will be a code
+  
+  return 1;
+}
