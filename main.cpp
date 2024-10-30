53bad32061d20941c068ca1b4862641da895720a
IFriendlyI <82224993+IFriendlyI@users.noreply.github.com>
2024-10-30 15:17:23 +0300

    Update main.cpp
    added variables and input the numbers

--- main.cpp
+++ main.cpp
@@ -10,6 +10,20 @@ int main()
   SetConsoleOutputCP(1251);
 
   // here will be a code
+  char num[100];
+  int sysNow = 2;
+  int sysAfter = 2;
+
+  cout << "Перевести число: ";
+  cin >> num;
+
+  cout << "Из системы: ";
+  cin >> sysNow;
+
+  cout << "В систему: ";
+  cin >> sysAft;
+
+  // here will be a conversion to new system
   
   return 1;
 }
